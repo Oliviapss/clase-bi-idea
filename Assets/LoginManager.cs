6f47b2cff15b9ec022b5b913fb6f1a8701381d56
Oliviapss <olivia1009palomino@hotmail.com>
2024-05-28 10:29:05 -0300

    password terminado

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoginManager : MonoBehaviour
+{
+    public string contraseña;
+    public InputField imputContraseña;
+
+    public void ComoDesees()
+    {
+        if (imputContraseña.text == contraseña)
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+    }
+}
